b9bb07a754792b5ffecc5d0a86dffa9db52499a2
prabirshrestha <mail@prabir.me>
2011-03-03 17:24:01 +0545

    + NUnit 2.5.9.10348

--- src/SimpleJson.Tests/SimpleJsonTests.cs
+++ src/SimpleJson.Tests/SimpleJsonTests.cs
@@ -1,4 +1,14 @@
 namespace SimpleJson.Tests
 {
+#if NUNIT
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+using TestCleanup = NUnit.Framework.TearDownAttribute;
+using TestInitialize = NUnit.Framework.SetUpAttribute;
+using ClassCleanup = NUnit.Framework.TestFixtureTearDownAttribute;
+using ClassInitialize = NUnit.Framework.TestFixtureSetUpAttribute;
+#else
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+#endif
 
 }
