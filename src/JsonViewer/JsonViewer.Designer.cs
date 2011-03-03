ab56084a5e59c85e937e2249342c202130615db8
prabirshrestha <mail@prabir.me>
2011-03-03 22:23:21 +0545

    + JsonViewer

---
+++ src/JsonViewer/JsonViewer.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace JsonViewer
+{
+    partial class JsonViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(JsonViewer));
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuClear = new System.Windows.Forms.ToolStripButton();
+            this.tvJson = new System.Windows.Forms.TreeView();
+            this.txtJson = new System.Windows.Forms.TextBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(12, 28);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(774, 141);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripSeparator1,
+            this.mnuClear});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(798, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(39, 22);
+            this.toolStripButton2.Text = "Copy";
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(39, 22);
+            this.toolStripButton3.Text = "Paste";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // mnuClear
+            // 
+            this.mnuClear.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.mnuClear.Image = ((System.Drawing.Image)(resources.GetObject("mnuClear.Image")));
+            this.mnuClear.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.mnuClear.Name = "mnuClear";
+            this.mnuClear.Size = new System.Drawing.Size(38, 22);
+            this.mnuClear.Text = "Clear";
+            this.mnuClear.Click += new System.EventHandler(this.mnuClear_Click);
+            // 
+            // tvJson
+            // 
+            this.tvJson.Location = new System.Drawing.Point(12, 175);
+            this.tvJson.Name = "tvJson";
+            this.tvJson.Size = new System.Drawing.Size(521, 339);
+            this.tvJson.TabIndex = 2;
+            // 
+            // txtJson
+            // 
+            this.txtJson.Location = new System.Drawing.Point(539, 175);
+            this.txtJson.Multiline = true;
+            this.txtJson.Name = "txtJson";
+            this.txtJson.ReadOnly = true;
+            this.txtJson.Size = new System.Drawing.Size(247, 339);
+            this.txtJson.TabIndex = 3;
+            this.txtJson.TextChanged += new System.EventHandler(this.txtJson_TextChanged);
+            // 
+            // JsonViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 526);
+            this.Controls.Add(this.txtJson);
+            this.Controls.Add(this.tvJson);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.txtInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "JsonViewer";
+            this.Text = "JsonViewer - powered by Simple Json (http://simplejson.codeplex.com)";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton mnuClear;
+        private System.Windows.Forms.TreeView tvJson;
+        private System.Windows.Forms.TextBox txtJson;
+    }
+}
+
---
+++ src/JsonViewer/JsonViewer.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace JsonViewer
+{
+    public partial class JsonViewer : Form
+    {
+        private Color defaultColor;
+
+        public JsonViewer()
+        {
+            InitializeComponent();
+            defaultColor = txtJson.BackColor;
+        }
+
+        private void mnuClear_Click(object sender, EventArgs e)
+        {
+            txtInput.Text = string.Empty;
+        }
+
+        private void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            txtJson.Text = txtInput.Text;
+        }
+
+        private void txtJson_TextChanged(object sender, EventArgs e)
+        {
+            object obj;
+            tvJson.Nodes.Clear();
+            if (SimpleJson.SimpleJson.TryJsonDecode(txtJson.Text, out obj))
+            {
+                var rootNode = new TreeNode("JSON");
+                tvJson.Nodes.Add(rootNode);
+                AddNode(obj, rootNode);
+                txtJson.BackColor = this.defaultColor;
+            }
+            else if (txtJson.Text.Trim().Length != 0)
+            {
+                txtJson.BackColor = Color.IndianRed;
+            }
+        }
+
+        #region Node Helpers
+
+        private void AddNode(object obj, TreeNode node)
+        {
+            if (obj is IList<object>)
+            {
+                AddNode((IList<object>)obj, node);
+            }
+            else if (obj is IDictionary<string, object>)
+            {
+                AddNode((IDictionary<string, object>)obj, node);
+            }
+            else
+            {
+                if (obj == null)
+                {
+                    node.Nodes.Add("null");
+                }
+                else
+                {
+                    node.Nodes.Add(obj.ToString());
+                }
+            }
+        }
+
+        private void AddNode(IDictionary<string, object> dictionary, TreeNode node)
+        {
+            if (dictionary.Count == 0)
+            {
+                node.Nodes.Add("{}");
+            }
+            else
+            {
+                foreach (var pair in dictionary)
+                {
+                    var key = pair.Key;
+                    if (pair.Value is IDictionary<string, object>)
+                    {
+                        var n = new TreeNode(pair.Key);
+                        node.Nodes.Add(n);
+                        AddNode(pair.Value, n);
+                    }
+                    else if (pair.Value is IList<object>)
+                    {
+                        var n = new TreeNode("[] " + pair.Key);
+                        node.Nodes.Add(n);
+                        AddNode((IList<object>)pair.Value, n);
+                    }
+                    else
+                    {
+                        // todo: differentiate null
+                        node.Nodes.Add(string.Format("{0} : {1}", pair.Key, pair.Value));
+                    }
+
+                    node.ExpandAll();
+                }
+            }
+        }
+
+        private void AddNode(IList<object> list, TreeNode node)
+        {
+            foreach (var o in list)
+            {
+                AddNode(o, node);
+            }
+        }
+
+        #endregion
+
+    }
+}
