6c5a7edbfbff565682cc94639715e8061413de59
prabirshrestha <mail@prabir.me>
2011-04-21 21:01:32 +0545

    + SafeDictionary

--- src/SimpleJson/SimpleJson.cs
+++ src/SimpleJson/SimpleJson.cs
@@ -1708,6 +1708,41 @@ namespace SimpleJson
                 }
             }
         }
+
+#if SIMPLE_JSON_INTERNAL
+    internal
+#else
+        public
+#endif
+ class SafeDictionary<TKey, TValue>
+        {
+            private readonly object _padlock = new object();
+            private readonly Dictionary<TKey, TValue> _dictionary = new Dictionary<TKey, TValue>();
+
+            public bool TryGetValue(TKey key, out TValue value)
+            {
+                return _dictionary.TryGetValue(key, out value);
+            }
+
+            public TValue this[TKey key]
+            {
+                get { return _dictionary[key]; }
+            }
+
+            public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+            {
+                return ((ICollection<KeyValuePair<TKey, TValue>>)_dictionary).GetEnumerator();
+            }
+
+            public void Add(TKey key, TValue value)
+            {
+                lock (_padlock)
+                {
+                    if (_dictionary.ContainsKey(key) == false)
+                        _dictionary.Add(key, value);
+                }
+            }
+        }
     }
 
     #endregion
