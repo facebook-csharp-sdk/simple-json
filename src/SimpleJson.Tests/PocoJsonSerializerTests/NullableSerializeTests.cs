cf2c8b59bd19d165f43fdf11096c18e009f3d572
prabirshrestha <mail@prabir.me>
2012-03-06 14:37:59 -0500

    Nullable type tests

---
+++ src/SimpleJson.Tests/PocoJsonSerializerTests/NullableSerializeTests.cs
@@ -0,0 +1,45 @@
+﻿namespace SimpleJsonTests.PocoJsonSerializerTests
+{
+#if NUNIT
+    using TestClass = NUnit.Framework.TestFixtureAttribute;
+    using TestMethod = NUnit.Framework.TestAttribute;
+    using TestCleanup = NUnit.Framework.TearDownAttribute;
+    using TestInitialize = NUnit.Framework.SetUpAttribute;
+    using ClassCleanup = NUnit.Framework.TestFixtureTearDownAttribute;
+    using ClassInitialize = NUnit.Framework.TestFixtureSetUpAttribute;
+    using NUnit.Framework;
+#else
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+#endif
+
+    [TestClass]
+    public class NullableSerializeTests
+    {
+        [TestMethod]
+        public void SerializeNullableTypeThatIsNotNull()
+        {
+            var obj = new NullableTypeClass();
+            obj.Value = null;
+
+            var json = SimpleJson.SimpleJson.SerializeObject(obj);
+
+            Assert.AreEqual("{\"Value\":null}", json);
+        }
+
+        [TestMethod]
+        public void SerializeNullableTypeThatIsNull()
+        {
+            var obj = new NullableTypeClass();
+            obj.Value = 4;
+
+            var json = SimpleJson.SimpleJson.SerializeObject(obj);
+
+            Assert.AreEqual("{\"Value\":4}", json);
+        }
+
+        public class NullableTypeClass
+        {
+            public int? Value { get; set; }
+        }
+    }
+}
