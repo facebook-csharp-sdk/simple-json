1d04231e8f9e708aa1bf77bb4620e83faeed22f0
prabirshrestha <mail@prabir.me>
2012-03-16 09:55:48 -0400

    DateTimeSerializeTests

--- src/SimpleJson.Tests/SerializeObject.UnknownNonPrimitive.Tests.cs
+++ src/SimpleJson.Tests/SerializeObject.UnknownNonPrimitive.Tests.cs
@@ -104,11 +104,12 @@ namespace SimpleJsonTests
         {
             var instance = new
             {
-                now = DateTime.UtcNow
+                now = new DateTime(2004, 1, 20, 5, 3, 6, DateTimeKind.Utc)
             };
 
             var json = SimpleJson.SerializeObject(instance);
             Assert.IsNotNull(json);
+            Assert.AreEqual("{\"now\":\"2004-01-20T05:03:06.0000000Z\"}", json);
         }
 
         [TestMethod]
