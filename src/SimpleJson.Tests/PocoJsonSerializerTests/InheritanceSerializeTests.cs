7a6edf8650cfeb9f6d8fdafd74e093fef157dfa4
prabirshrestha <mail@prabir.me>
2014-02-27 18:51:54 -0500

    added failing inheritence test

---
+++ src/SimpleJson.Tests/PocoJsonSerializerTests/InheritanceSerializeTests.cs
@@ -0,0 +1,67 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="InheritanceSerializeTests.cs" company="The Outercurve Foundation">
+//    Copyright (c) 2011, The Outercurve Foundation.
+//
+//    Licensed under the MIT License (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//      http://www.opensource.org/licenses/mit-license.php
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// </copyright>
+// <author>Nathan Totten (ntotten.com), Jim Zimmerman (jimzimmerman.com) and Prabir Shrestha (prabir.me)</author>
+// <website>https://github.com/facebook-csharp-sdk/simple-json</website>
+//-----------------------------------------------------------------------
+
+namespace SimpleJson.Tests.PocoJsonSerializerTests
+{
+    using System;
+
+#if NUNIT
+    using TestClass = NUnit.Framework.TestFixtureAttribute;
+    using TestMethod = NUnit.Framework.TestAttribute;
+    using TestCleanup = NUnit.Framework.TearDownAttribute;
+    using TestInitialize = NUnit.Framework.SetUpAttribute;
+    using ClassCleanup = NUnit.Framework.TestFixtureTearDownAttribute;
+    using ClassInitialize = NUnit.Framework.TestFixtureSetUpAttribute;
+    using NUnit.Framework;
+#else
+#if NETFX_CORE
+    using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+#else
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+#endif
+#endif
+
+    [TestClass]
+    public class InheritanceSerializeTests
+    {
+        [TestMethod]
+        public void SerializeShouldContainInheritedProperties()
+        {
+            var dog = new Dog
+            {
+                Type = "dog",
+                Legs = 4
+            };
+
+            var actual = SimpleJson.SerializeObject(dog);
+            var expected = "{\"Legs\":4,\"Type\":\"dog\"}";
+            Assert.AreEqual(expected, actual);
+        }
+
+        public class Animal
+        {
+            public string Type { get; set; }
+        }
+
+        public class Dog : Animal
+        {
+            public int Legs { get; set; }
+        }
+    }
+}
